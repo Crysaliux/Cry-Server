6f0bb71b28676398de6c6fa8478ae225e25a1c45
Mester <mechaqwerbear@gmail.com>
2025-06-10 21:17:41 +0400

    modified:   Client/server/src/layouts/popup_widgets/group_action_menu_widget/group_action_menu_layout.tsx

--- Client/server/src/layouts/popup_widgets/group_action_menu_widget/group_action_menu_layout.tsx
+++ Client/server/src/layouts/popup_widgets/group_action_menu_widget/group_action_menu_layout.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { GlobalContext } from "services/global_manager";
 import GroupActionMenuView from './views/group_action_menu_view';
 
-const GrupActionMenuLayout: React.FC = () => {
+const GroupActionMenuLayout: React.FC = () => {
     const context_data = useContext(GlobalContext);
     if (!context_data) {
         throw new Error("Context for channels_view can't be defined.");
@@ -20,3 +20,5 @@ const GrupActionMenuLayout: React.FC = () => {
         </>
     );
 };
+
+export default React.memo(GroupActionMenuLayout);
