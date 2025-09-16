cb7d926ab2cd1e82120e09011e367fea964affac
Crysaliux <mechaqwerbear@gmail.com>
2025-09-16 22:24:22 +0400

    modified:   Core/services/oauth.py

--- Core/services/oauth.py
+++ Core/services/oauth.py
@@ -87,7 +87,7 @@ class Authentication:
             email=email,
             date_of_birth=date.fromisoformat(date_of_birth),
             token=access_token, 
-            token_expires_at=access_expires_at, 
+            token_expires_at=access_expires_at.timestamp(), #check
             id=client_id))
         
         await session.commit()
