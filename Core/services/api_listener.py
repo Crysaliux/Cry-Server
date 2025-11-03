2f756a09cafafe3319710af66a8f8acd0f5a9fef
Crysaliux <mechaqwerbear@gmail.com>
2025-11-03 22:05:51 +0400

    modified:   Core/services/api_listener.py

--- Core/services/api_listener.py
+++ Core/services/api_listener.py
@@ -29,7 +29,7 @@ class RoomRelated(BaseModel):
     group_id: str
     room_id: str
 
-class RoleRelated(BaseModel):
+class RoleRelated(BaseModel): #Rethink & Refactor!
     group_id: str
     room_id: str
     role_id: str
